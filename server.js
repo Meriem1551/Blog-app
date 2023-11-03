e5d0dd62c6109ffec74700c29b8c0155d1da9c09
Meriem1551 <meriem200409@gmail.com>
2023-11-03 20:21:21 +0100

    Creating multiple pages

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
      console.log(req.url, req.method);
 
      //set header ontent type
-     res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/html');
+    //  res.write('<p>Hello, ninjas</p>');
+    //  res.write('<p>Hello again, ninjas</p>');
+    //  res.end();
 
-     res.write('<p>Hello, ninjas</p>');
-     res.write('<p>Hello again, ninjas</p>');
+    let path  = './views';
+    switch(req.url){
+        case '/':
+            path += '/index.html'
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += '/about.html'
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '/404page.html'
+            res.statusCode = 404;
+            break;
+    }
+    // send an html file
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.write(data);
+        }
+        res.end();
+    })
 
-     res.end();
 });
 
 server.listen(3000, 'localhost', () => {
