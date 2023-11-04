55d4673af0a6c0ca5caecb5f7b84e49a8a5cf4c0
Meriem1551 <meriem200409@gmail.com>
2023-11-04 18:50:13 +0100

    Adding ejs files

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
 //express app
 const app = express();
+
+app.set('view engine', 'ejs');
 
 //listen for requests
 
@@ -9,21 +10,23 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
     // res.send('<p>About page</p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
+
 
 })
 
 //redirect
-app.get('/about-us', (req, res) =>{
-    res.redirect('/about');
+
+app.get('./blogs/create', (req, res) =>{
+    res.render('create');
 })
 
 //404 page
 app.use((req, res) =>{
-    res.status(404).sendFile('./views/404page.html', {root: __dirname});
+    res.status(404).render('404page');
 })
